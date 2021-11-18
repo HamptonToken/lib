50799122833c20e1fedc891bb458fd786a42fdbb
hampton <83888722+HamptonToken@users.noreply.github.com>
2021-11-18 11:11:03 -0800

    ECDSA cryptography

---
+++ ecdsa.go
@@ -0,0 +1,35 @@
+package sign
+
+import (
+	"crypto"
+	"crypto/x509"
+	"crypto/rsa"
+	"crypto/ecdsa"
+	"fmt"
+	"math/big"
+	"errors"
+	"crypto/sha256"
+	"crypto/rand"
+	"encoding/pem"
+)
+
+
+func EcdsaSign(privpem, input string) (rSig, sSig string, err error) {
+        sum := sha256.Sum256([]byte(input))
+        priv, err := parseEcdsaPrivateKeyFromPemStr(privpem)
+        if (err != nil) {
+                fmt.Println(err)
+                return "", "", err
+        }
+
+        r, s, err := ecdsa.Sign(rand.Reader, priv, sum[:32])
+
+        if err != nil {
+                fmt.Println(err)
+                return  "", "", err
+        }
+
+        return r.String(), s.String(), nil
+
+}
+
