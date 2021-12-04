71f4066c6407c26e1b8acb8200366d74f3eab036
hampton <83888722+HamptonToken@users.noreply.github.com>
2021-12-04 09:21:49 -0800

    Update util.go

--- util.go
+++ util.go
@@ -29,3 +29,18 @@ func parseRsaPrivateKeyFromPemStr(privPEM string) (*rsa.PrivateKey, error) {
 
     return priv, nil
 }
+
+func parseRsaPrivateKeyFromPemStr(privPEM string) (*rsa.PrivateKey, error) {
+    block, _ := pem.Decode([]byte(privPEM))
+    if block == nil {
+            return nil, errors.New("failed to parse PEM block containing the privkey")
+    }
+
+    priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+    if err != nil {
+            return nil, err
+    }
+
+    return priv, nil
+}
+
