d7dcb73deb648dab1b7002ea248f779bda5f0334
hampton <83888722+HamptonToken@users.noreply.github.com>
2022-05-17 13:48:34 -0700

    Update store.go

--- store.go
+++ store.go
@@ -11,3 +11,13 @@ import (
 )
 
 /* high level APIs here */
+
+/*
+>>> import ipfsApi
+>>> api = ipfsApi.Client('127.0.0.1', 5001)
+>>> res = api.add('test.txt')
+>>> res
+{'Hash': 'QmWxS5aNTFEc9XbMX1ASvLET1zrqEaTssqt33rVZQCQb22', 'Name': 'test.txt'}
+>>> api.cat(res['Hash'])
+'fdsafkljdskafjaksdjf\n'
+*/
