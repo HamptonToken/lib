236df4f8054acaf5e9b72dfaf67f1b80c1702c04
hampton <83888722+HamptonToken@users.noreply.github.com>
2022-05-18 09:20:26 -0700

    Update store.go

--- store.go
+++ store.go
@@ -21,3 +21,22 @@ import (
 >>> api.cat(res['Hash'])
 'fdsafkljdskafjaksdjf\n'
 */
+
+/*
+const node = await IPFS.create()
+
+const data = 'Hello, <YOUR NAME HERE>'
+
+// add your data to to IPFS - this can be a string, a Buffer,
+// a stream of Buffers, etc
+const results = node.add(data)
+
+// we loop over the results because 'add' supports multiple 
+// additions, but we only added one entry here so we only see
+// one log line in the output
+for await (const { cid } of results) {
+  // CID (Content IDentifier) uniquely addresses the data
+  // and can be used to get it again.
+  console.log(cid.toString())
+}
+*/
