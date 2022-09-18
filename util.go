4843111bbb4f3e62ea0c860743976bd30c20ee67
hampton <83888722+HamptonToken@users.noreply.github.com>
2022-09-18 00:09:33 -0700

    Update util.go

--- util.go
+++ util.go
@@ -89,7 +89,7 @@ design risk model
 policy
 enforcement function (related to the policies. need a mapping)
 assessment of policy
-Compliance in different regions
+compliance in different regions
 */
 
 /* util functions */
