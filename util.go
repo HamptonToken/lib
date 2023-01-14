347240d8b2d4d34f05840118d068bd36a881ce52
hampton <83888722+HamptonToken@users.noreply.github.com>
2023-01-14 12:53:27 -0800

    Update util.go

--- util.go
+++ util.go
@@ -72,7 +72,7 @@ four categories: crypto user, non-crypto user, crypto developer, non-crypto deve
 don't need to mention privacy too much.
 maybe not focus on personal dada. otherwise zero knowledge is not enough
 
-new design
+new design with new doc
 ============================================================================================================
 cases for encryption
 1. encrption: profile, message, authorizaion list?
