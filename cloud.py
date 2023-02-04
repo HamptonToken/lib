b95d548fbe309472ded9c07bdb3b707b7f2ab886
hampton <83888722+HamptonToken@users.noreply.github.com>
2023-02-04 02:19:38 -0800

    Update cloud.py

--- cloud.py
+++ cloud.py
@@ -13,9 +13,14 @@ Amazon Mechanical Turk (MTurk) is a crowdsourcing website for businesses to hire
                                     or the more limited MTurk Requester site. As of April 2019, Requesters could register from 49 approved countries.
 
 
-
-
-
+AWS Elastic Beanstalk is an orchestration service offered by Amazon Web Services for deploying applications which orchestrates various AWS services, 
+            including EC2, S3, Simple Notification Service (SNS), CloudWatch, autoscaling, and Elastic Load Balancers. Elastic Beanstalk provides an 
+                        additional layer of abstraction over the bare server and OS; users instead see a pre-built combination of OS and platform, 
+                              such as "64bit Amazon Linux 2014.03 v1.1.0 running Ruby 2.0 (Puma)" or "64bit Debian jessie v2.0.7 running Python 3.4 (Preconfigured - Docker)".
+                                    Deployment requires a number of components to be defined: an 'application' as a logical container for the project, a 'version' 
+                                          which is a deployable build of the application executable, a 'configuration template' that contains configuration information for both 
+                                                the Beanstalk environment and for the product.[4] Finally an 'environment' combines a 'version' with a 'configuration' and 
+                                                      deploys them.[3] Executables themselves are uploaded as archive files to S3 beforehand and the 'version' is just a pointer to this.
 
 
 
