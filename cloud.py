6cb2646335c192ea6df14c1faa4663f2537556cb
hampton <83888722+HamptonToken@users.noreply.github.com>
2023-02-04 02:14:40 -0800

    Update cloud.py

--- cloud.py
+++ cloud.py
@@ -1,1 +1,23 @@
+/*
+1, SQS, S3, EC2
+2,  SimpleDB, Mechanical Turk, Elastic Block Store, Elastic Beanstalk, Relational Database Service, DynamoDB, 
+      CloudWatch, Simple Workflow, CloudFront, and Availability Zones
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
