7c36acdb859a2a908475d41dc527eb1fd5265ea1
hampton <83888722+HamptonToken@users.noreply.github.com>
2023-02-04 02:16:45 -0800

    Update cloud.py

--- cloud.py
+++ cloud.py
@@ -4,7 +4,13 @@
       CloudWatch, Simple Workflow, CloudFront, and Availability Zones
 
 
-
+Amazon Mechanical Turk (MTurk) is a crowdsourcing website for businesses to hire remotely located "crowdworkers" to 
+      perform discrete on-demand tasks that computers are currently unable to do. It is operated under Amazon Web Services, 
+            and is owned by Amazon. Employers (known as requesters) post jobs known as Human Intelligence Tasks (HITs), 
+                  such as identifying specific content in an image or video, writing product descriptions, or answering survey questions. 
+                        Workers, colloquially known as Turkers or crowdworkers, browse among existing jobs and complete them in exchange for 
+                              a fee set by the employer. To place jobs, the requesting programs use an open application programming interface (API), 
+                                    or the more limited MTurk Requester site. As of April 2019, Requesters could register from 49 approved countries.
 
 
 
