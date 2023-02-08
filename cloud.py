af62aae8718af5a74551f2f04527d1a63d672ac8
hampton <83888722+HamptonToken@users.noreply.github.com>
2023-02-07 23:46:34 -0800

    Update cloud.py

--- cloud.py
+++ cloud.py
@@ -54,7 +54,11 @@ Comparison Table of Low-code Platforms
 #19) FileMaker
 
 
-
+AppSheet is an application that provides a no-code development platform for application software, which allows users to create mobile, tablet, 
+      and web applications using data sources like Google Drive, DropBox, Office 365, and other cloud-based spreadsheet and database platforms. 
+            The platform can be utilized for a broad set of business use cases including project management, customer relationship management, 
+                  field inspections, and personalized reporting.
+AppSheet was acquired by Google in January 2020.
 
 
 
