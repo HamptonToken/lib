f14d35e27fd847b4e49594b0df5a0d418ee9f298
hampton <83888722+HamptonToken@users.noreply.github.com>
2023-03-21 00:00:24 -0700

    Update cloud.py

--- cloud.py
+++ cloud.py
@@ -76,5 +76,6 @@ react graphGL structure: over websocket, ajax
 meeting notes:
       1. focus on user cases
       2. mastodon (Mastodon is maintained by a network of independent servers that are managed by individual users or groups.)
+      3. timeline, hashtag
 
 */
