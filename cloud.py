6b4b9e726a162fb573cdc3fa49274de493e14865
hampton <83888722+HamptonToken@users.noreply.github.com>
2023-08-03 12:56:01 +0800

    Update cloud.py

--- cloud.py
+++ cloud.py
@@ -1,7 +1,7 @@
 /*
 1, SQS, S3, EC2
 2,  SimpleDB, Mechanical Turk, Elastic Block Store, Elastic Beanstalk, Relational Database Service, DynamoDB, 
-      CloudWatch, Simple Workflow, CloudFront, and Availability Zones.
+      CloudWatch, Simple Workflow, CloudFront, and Availability Zones..
 
 
 Amazon Mechanical Turk (MTurk) is a crowdsourcing website for businesses to hire remotely located "crowdworkers" to 
